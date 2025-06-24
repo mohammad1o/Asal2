6a01b285acd2dceea67ad75d06b7876c699f74bf
mohammad khuffash <khuffashmohammad423@gmail.com>
2025-06-25 00:48:17 +0300

    this is first push js

---
+++ script.js
@@ -0,0 +1,30 @@
+let maininput = document.getElementById("mainInput");
+
+let warrning = document.getElementById("warning");
+
+let addTask= document.getElementById("InputButton");
+
+let Btall=document.getElementById("All");
+
+let BtDone=document.getElementById("Done");
+
+let BtToDo=document.getElementById("ToDo");
+
+let notasks=document.getElementById("noTasksHeader");
+
+let scrolltasks=document.getElementById("scrollContainer");
+
+let BtdeletDone=document.getElementById("deletDone");
+
+let BtdeletAll=document.getElementById("deletAll");
+
+let editpupop=document.getElementById("editpopup");
+let editInput=document.getElementById("editInput");
+let saveBt=document.getElementById("saveBt");
+let CancelBt1=document.getElementById("CancelBt1");
+let deletpopup=document.getElementById("deletpopup");
+let inputdelet=document.getElementById("inputdelet");
+let confirmBt=document.getElementById("confirmBt");
+let CancelBt2=document.getElementById("CancelBt2");
+let newEdit=null;
+let makeDelete=null;
